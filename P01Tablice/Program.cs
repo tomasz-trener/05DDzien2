da568c885e3e75e735b9941b40bf058eacde42c5
kurs <kurs@DESKTOP-V3HDKPQ>
2021-09-09 08:37:45 +0200

    Add project files.

---
+++ P01Tablice/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P01Tablice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a1 = 5;
+            int a2 = 6;
+            int a3 = 8;
+
+            int[] tablica = new int[3];
+            tablica[0] = 5;
+            tablica[1] = 6;
+            // tablica[2] = 8;
+
+            int a4 = tablica[1];
+
+            int[] t2 = new int[5];
+
+            int ileImion = Convert.ToInt32(Console.ReadLine());
+            string[] t3 = new string[ileImion];
+
+            int dlugoscTablicy = t3.Length;
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ P02ZadanieTablice/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P02ZadanieTablice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj ile imion chcesz przechowywać");
+            int ile = Convert.ToInt32(Console.ReadLine());
+
+            string[] imiona = new string[ile];
+
+            Console.WriteLine("Zacznij podawać imiona i zaakceptuj ENTER");
+
+            for (int i = 0; i < ile; i++)
+                imiona[i] = Console.ReadLine();
+
+            // teraz bezdiemy wypisywac imiona 
+
+            for (int i = 0; i < ile; i++)
+            {
+                string wynik = String.Format("{0} ({1})", imiona[i], imiona[i].Length);
+                Console.WriteLine(wynik);
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
